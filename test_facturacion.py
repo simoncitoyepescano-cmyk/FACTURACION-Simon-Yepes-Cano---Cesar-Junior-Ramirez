ad80e6e80d7ca36b00a4191ee996f08781fed753
SIMON YEPES <simoncitoyepescano@gmail.com>
2026-07-31 07:45:08 -0500

    test_facturacion.py

--- test_facturacion.py
+++ test_facturacion.py
@@ -2,27 +2,24 @@ import unittest
 import facturacion_sensores
 
 class TestsFacturacion(unittest.TestCase):
-  #GAMMA
-  def comprobar_valor_gamma(self): 
-  #Establecer datos de entrada
-    n_servicios: int = 1
-    precio_unitario: float = 1741500
-    iva: float = 330885
+    #GAMMA
+    def test_comprobar_valor_gamma(self): 
+    #Establecer datos de entrada
+        n_servicios: int = 1
+        precio_unitario: float = 1741500
+        iva: float = 330885
 
-    #Establecer datos de salida esperada
-    valor_total_esperado = 2072385
+        #Establecer datos de salida esperada
+        valor_total_esperado = 2072385
 
-    #Probar la funcion que resuelve problemas
-    valor_calculado = facturacion_sensores.calcular_valor_factura(n_servicios,precio_unitario,iva)
+        #Probar la funcion que resuelve problemas
+        valor_calculado = facturacion_sensores.calcular_valor_factura(n_servicios,precio_unitario,iva)
 
-    # Verificar que el dato obtenido sea el esperado
-    if valor_calculado == valor_total_esperado:
-        print("La factura de GAMMA esta bien")
-    else:
-        print("Hay un error en la factura")
+        # Verificar que el dato obtenido sea el esperado
+        self.assertEqual(valor_calculado,valor_total_esperado)
     
     #CORLANC
-    def comprobar_valor_corlanc(self): 
+    def test_comprobar_valor_corlanc(self): 
     #Establecer datos de entrada
         n_servicios: int = 2
         precio_unitario: float = 241875
@@ -33,36 +30,30 @@ class TestsFacturacion(unittest.TestCase):
         valor_total = round(valor_total_esperado)
 
         #Probar la funcion que resuelve problemas
-        valor_calculado = facturacion_sensores.calcular_valor_factura_corlanc(n_servicios,precio_unitario,iva)
+        valor_calculado = facturacion_sensores.calcular_valor_factura(n_servicios,precio_unitario,iva)
 
         # Verificar que el dato obtenido sea el esperado
-        if round(valor_calculado) == valor_total:
-            print("La factura de CORLANC esta bien")
-        else:
-            print("Hay un error en la factura")
+        self.assertAlmostEqual(valor_calculado,valor_total,0)
 
     #LOCERIA
-    def comprobar_valor_loceria(self):
+    def test_comprobar_valor_loceria(self):
+
         #Establecer datos de entrada
         n_servicios: int = 1
         precio_unitario: float = 3483000
         iva: float = 661770
 
         #Establecer datos de salida esperada
-        valor_total_esperado = 4144770
-        valor_total = round(valor_total_esperado)
+        valor_total_esperado =  4144770
 
         #Probar la funcion que resuelve problemas
-        valor_calculado = facturacion_sensores.calcular_valor_factura_corlanc(n_servicios,precio_unitario,iva)
+        valor_calculado = facturacion_sensores.calcular_valor_factura(n_servicios,precio_unitario,iva)
 
         # Verificar que el dato obtenido sea el esperado
-        if round(valor_calculado) == valor_total:
-            print("La factura de LOCERIA esta bien")
-        else:
-            print("Hay un error en la factura")
+        self.assertAlmostEqual(valor_calculado,valor_total_esperado)
 
     #ENKA
-    def comprobar_valor_enka(self):
+    def test_comprobar_valor_enka(self):
         #Establecer datos de entrada
         n_servicios: int = 1
         precio_unitario: float = 1680000
@@ -70,39 +61,31 @@ class TestsFacturacion(unittest.TestCase):
 
         #Establecer datos de salida esperada
         valor_total_esperado = 1999200
-        valor_total = round(valor_total_esperado)
 
         #Probar la funcion que resuelve problemas
-        valor_calculado = facturacion_sensores.calcular_valor_factura_corlanc(n_servicios,precio_unitario,iva)
+        valor_calculado = facturacion_sensores.calcular_valor_factura(n_servicios,precio_unitario,iva)
 
         # Verificar que el dato obtenido sea el esperado
-        if round(valor_calculado) == valor_total:
-            print("La factura de ENKA esta bien")
-        else:
-            print("Hay un error en la factura")
+        self.assertEqual(valor_calculado,valor_total_esperado)
 
     #CRYOGAS GATEWAYS
-    def comprobar_valor_cryogas_gateways(self):
+    def test_comprobar_valor_cryogas_gateways(self):
         #Establecer datos de entrada
         n_servicios: int = 1
         precio_unitario: float = 6820000
         iva: float = 1295800
 
         #Establecer datos de salida esperada
-        valor_total_esperado = 8115800  
-        valor_total = round(valor_total_esperado)
+        valor_total_esperado = 8115800
 
         #Probar la funcion que resuelve problemas
-        valor_calculado = facturacion_sensores.calcular_valor_factura_corlanc(n_servicios,precio_unitario,iva)
+        valor_calculado = facturacion_sensores.calcular_valor_factura(n_servicios,precio_unitario,iva)
 
         # Verificar que el dato obtenido sea el esperado
-        if round(valor_calculado) == valor_total:
-            print("La factura de CRYOGAS gateway esta bien")
-        else:
-            print("Hay un error en la factura")
+        self.assertEqual(valor_calculado,valor_total_esperado)
 
     #CRYOGAS SENSOR
-    def comprobar_valor_cryogas_sensor(self):
+    def test_comprobar_valor_cryogas_sensor(self):
         #Establecer datos de entrada
         n_servicios: int = 1
         precio_unitario: float = 1500000
@@ -110,16 +93,12 @@ class TestsFacturacion(unittest.TestCase):
 
         #Establecer datos de salida esperada
         valor_total_esperado = 1785000
-        valor_total = round(valor_total_esperado)
 
         #Probar la funcion que resuelve problemas
-        valor_calculado = facturacion_sensores.calcular_valor_factura_corlanc(n_servicios,precio_unitario,iva)
+        valor_calculado = facturacion_sensores.calcular_valor_factura(n_servicios,precio_unitario,iva)
 
         # Verificar que el dato obtenido sea el esperado
-        if round(valor_calculado) == valor_total:
-            print("La factura de CRYOGAS sensor esta bien")
-        else:
-            print("Hay un error en la factura")
+        self.assertEqual(valor_calculado,valor_total_esperado)
 
 if __name__ == '__main__':
     unittest.main()
