055713f61598a79ff704dfdbc993fcf03e5a3ff8
SIMON YEPES <simoncitoyepescano@gmail.com>
2026-08-01 20:06:39 -0500

    Delete facturacion_sensores.py

--- facturacion_sensores.py
+++
@@ -1,4 +0,0 @@
-def calcular_valor_factura(n_servicios:int,precio_unitario:float,iva: float):
-  iva = 0.19 * (n_servicios * precio_unitario)
-  valor_servicios = (n_servicios * precio_unitario) + iva
-  return valor_servicios
