4e3aef85ea5697059b3f5f3f2a819c8376239a00
cesarramirez16 <cesarjuniorramirezgarcia@gmail.com>
2026-08-12 16:03:01 -0500

    calcular_valor_factura

--- facturacion_sensores.py
+++ facturacion_sensores.py
@@ -2,18 +2,14 @@ class ServiciosInvalidos(Exception):
   pass
 class PrecioInvalido(Exception):
   pass
-class IvaInvalido(Exception):
-  pass
 
-def calcular_valor_factura(n_servicios:int,precio_unitario:float,iva: float):
+def calcular_valor_factura(n_servicios:int,precio_unitario:float):
   iva = 0.19 * (n_servicios * precio_unitario)
   valor_servicios = (n_servicios * precio_unitario) + iva
 
-  if n_servicios < 0:
-    raise ServiciosInvalidos("La cantidad de servicios no pueden ser negativos")
+  if n_servicios <= 0:
+    raise ServiciosInvalidos("La cantidad de servicios debe ser mayor que 0")
   if precio_unitario <= 0:
     raise PrecioInvalido("El precio unitario no puede ser negativos o 0")
-  if iva <= 0:
-    raise IvaInvalido("El iva no puede ser negativos o 0")
 
   return valor_servicios
