58b6686c393fa42de5cf795050c856d3ad40fc04
cesarramirez16 <cesarjuniorramirezgarcia@gmail.com>
2026-08-19 07:09:31 -0500

    Refactor function and variable names in facturacion_sensores.py

--- FACTURACION_S/src/model/facturacion_sensores.py
+++ FACTURACION_S/src/model/facturacion_sensores.py
@@ -3,11 +3,11 @@ class ServiciosInvalidos(Exception):
 class PrecioInvalido(Exception):
   pass
 
-def calcular_valor_factura(n_servicios:int,precio_unitario:float):
-  iva = 0.19 * (n_servicios * precio_unitario)
-  valor_servicios = (n_servicios * precio_unitario) + iva
+def calcular_valor_factura(numero_servicios:int,precio_unitario:float):
+  iva = 0.19 * (numero_servicios * precio_unitario)
+  valor_servicios = (numero_servicios * precio_unitario) + iva
 
-  if n_servicios <= 0:
+  if numero_servicios <= 0:
     raise ServiciosInvalidos("La cantidad de servicios debe ser mayor que 0")
   if precio_unitario <= 0:
     raise PrecioInvalido("El precio unitario no puede ser negativos o 0")
